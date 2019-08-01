e8a3155fe4e20dba450a7bcc1a850a8deacc5671
Muhammad Iqbal Pradipta <feetbonike90@gmail.com>
2019-08-01 11:30:23 +0700

    login

--- app/src/main/java/android/kotlin/belajar/belajarandroidprogrammingmaster4/MainActivity.kt
+++ app/src/main/java/android/kotlin/belajar/belajarandroidprogrammingmaster4/MainActivity.kt
@@ -2,11 +2,23 @@ package android.kotlin.belajar.belajarandroidprogrammingmaster4
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.login.*
 
 class MainActivity : AppCompatActivity() {
+
+    var user: String? = ""
+    var pass: String? = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn.setOnClickListener {
+            user = username.text.toString()
+            pass = password.text.toString()
+            Toast.makeText(this, "Ini username : $user dan password : $pass", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
